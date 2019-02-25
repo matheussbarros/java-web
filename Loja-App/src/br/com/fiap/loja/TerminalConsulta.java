e85af2e5c564ba408dc38017cfee06a068fb6ae2
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-25 10:12:24 -0300

    Criando Servico

---
+++ Loja-App/src/br/com/fiap/loja/TerminalConsulta.java
@@ -0,0 +1,27 @@
+package br.com.fiap.loja;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import br.com.fiap.loja.bo.EstoqueBO;
+import br.com.fiap.loja.singleton.PropertySingleton;
+import br.com.fiap.loja.to.ProdutoTO;
+
+public class TerminalConsulta {
+
+	public static void main(String[] args) {
+		String nome = PropertySingleton;
+		LocalDate hoje = LocalDate.now();
+		DateTimeFormatter formatador = 	DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		System.out.println(nome + " " + hoje.format(formatador));
+		System.out.println("Digite o código do produto desejado: ");
+		Scanner scanner = new Scanner(System.in);
+		int codigo = scanner.nextInt();
+		scanner.close();
+		
+		ProdutoTO produto = EstoqueBO.consultarProduto(401);
+		
+	}
+
+} 
---
+++ Loja-App/src/br/com/fiap/loja/to/ProdutoTO.java
@@ -0,0 +1,72 @@
+package br.com.fiap.loja.to;
+
+public class ProdutoTO {
+
+	private int codigo;
+	private String descricao;
+	private int quantidade;
+	private double preco;
+	
+	
+	public ProdutoTO(int codigo, String descricao, int quantidade, double preco) {
+		super();
+		this.codigo = codigo;
+		this.descricao = descricao;
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+
+
+	public ProdutoTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+
+	public double getPreco() {
+		return preco;
+	}
+
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
